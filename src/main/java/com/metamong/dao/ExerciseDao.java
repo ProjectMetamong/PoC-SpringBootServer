77029a3b75920912a72f587cd0fddb2ef580bc69
junkyu.lim <junkyu.lim.dev@gmail.com>
2021-06-02 09:17:15 +0900

    추가 : exercise dao 필요 repo method 추가

--- src/main/java/com/metamong/dao/ExerciseDao.java
+++ src/main/java/com/metamong/dao/ExerciseDao.java
@@ -2,10 +2,14 @@ package com.metamong.dao;
 
 import com.metamong.entity.Exercise;
 
-public interface ExerciseDao {
-    void create(Exercise exercise);
+import java.util.ArrayList;
 
-    void getById(int id);
+public interface ExerciseDao {
+    int create(Exercise exercise) throws Exception;
+
+    Exercise getById(int id) throws Exception;
+
+    ArrayList<Exercise> getAll() throws Exception;
 
     void update(Exercise exercise);
 
